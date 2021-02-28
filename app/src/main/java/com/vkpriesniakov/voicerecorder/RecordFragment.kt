35438a29b47fc7c7793003c1de6d8a7787a52c22
JFK <yoshinori.sakaguchi91@gmail.com>
2021-02-28 02:48:24 +0200

    Implemented -Fragments Nav and UI -Animation -Permission request -Icon

--- app/src/main/java/com/vkpriesniakov/voicerecorder/RecordFragment.kt
+++ app/src/main/java/com/vkpriesniakov/voicerecorder/RecordFragment.kt
@@ -1,59 +1,139 @@
 package com.vkpriesniakov.voicerecorder
 
+import android.content.res.ColorStateList
 import android.os.Bundle
-import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.activity.result.contract.ActivityResultContracts
+import androidx.appcompat.app.AppCompatActivity
+import androidx.fragment.app.Fragment
+import androidx.navigation.NavController
+import androidx.navigation.Navigation
+import com.vkpriesniakov.voicerecorder.databinding.FragmentRecordBinding
+import com.vkpriesniakov.voicerecorder.utils.RECORD_PERMISSION
+import com.vkpriesniakov.voicerecorder.utils.ROTATION_ANGLE
+import com.vkpriesniakov.voicerecorder.utils.Utils
+import com.vkpriesniakov.voicerecorder.utils.Utils.Companion.checkIfPermissionGranted
 
-// TODO: Rename parameter arguments, choose names that match
-// the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
-private const val ARG_PARAM1 = "param1"
-private const val ARG_PARAM2 = "param2"
 
-/**
- * A simple [Fragment] subclass.
- * Use the [RecordFragment.newInstance] factory method to
- * create an instance of this fragment.
- */
-class RecordFragment : Fragment() {
-    // TODO: Rename and change types of parameters
-    private var param1: String? = null
-    private var param2: String? = null
+class RecordFragment : Fragment(), View.OnClickListener {
 
-    override fun onCreate(savedInstanceState: Bundle?) {
-        super.onCreate(savedInstanceState)
-        arguments?.let {
-            param1 = it.getString(ARG_PARAM1)
-            param2 = it.getString(ARG_PARAM2)
+    private lateinit var mNavController: NavController
+    private var mIsRecording: Boolean = false
+    private var mPermissionAllowed = false
+
+    private var _binding: FragmentRecordBinding? = null
+
+    // This property is only valid between onCreateView and
+// onDestroyView.
+    private val bdn get() = _binding!!
+
+
+    override fun onCreateView(
+        inflater: LayoutInflater,
+        container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        _binding = FragmentRecordBinding.inflate(inflater, container, false)
+        return bdn.root
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+        mPermissionAllowed = checkIfPermissionGranted(context)
+        mNavController = Navigation.findNavController(view)
+        bdn.recordListButton.setOnClickListener(this)
+        bdn.recordFab.setOnClickListener(this)
+    }
+
+    override fun onClick(v: View?) {
+        when (v?.id) {
+            R.id.record_list_button -> mNavController.navigate(R.id.action_recordFragment_to_audioListFragment)
+            R.id.record_fab -> {
+                if (mIsRecording) {
+                    //Stop record
+                    setAnimation(
+                        1f, R.drawable.ic_start_record,
+                        R.color.white, R.color.purple_200,
+                        ROTATION_ANGLE
+                    )
+                    mIsRecording = false
+                } else {
+                    if (checkPermission()) {
+                        setAnimation(
+                            1.2f, R.drawable.ic_baseline_stop_24,
+                            R.color.colorPrimary, R.color.colowDarkBlue,
+                            -ROTATION_ANGLE
+                        )
+
+                        //TODO: Start record
+
+                        mIsRecording = true
+                    }
+                }
+            }
         }
     }
 
-    override fun onCreateView(
-        inflater: LayoutInflater, container: ViewGroup?,
-        savedInstanceState: Bundle?
-    ): View? {
-        // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_record, container, false)
-    }
-
-    companion object {
-        /**
-         * Use this factory method to create a new instance of
-         * this fragment using the provided parameters.
-         *
-         * @param param1 Parameter 1.
-         * @param param2 Parameter 2.
-         * @return A new instance of fragment RecordFragment.
-         */
-        // TODO: Rename and change types and number of parameters
-        @JvmStatic
-        fun newInstance(param1: String, param2: String) =
-            RecordFragment().apply {
-                arguments = Bundle().apply {
-                    putString(ARG_PARAM1, param1)
-                    putString(ARG_PARAM2, param2)
+    private val requestPermissionLauncher =
+        registerForActivityResult(
+            ActivityResultContracts.RequestPermission()
+        ) { isGranted: Boolean ->
+            when {
+                isGranted -> mPermissionAllowed = true
+                !isGranted -> {
+                    mPermissionAllowed = false
+                    if (!shouldShowRequestPermissionRationale(RECORD_PERMISSION))
+//                        showSettingsSnackbar()
+                        Utils.showSettingsSnackbar(
+                            bdn,
+                            activity as AppCompatActivity,
+                            activitySettingsResult
+                        )
                 }
             }
+        }
+
+    private fun checkPermission(): Boolean {
+        requestPermissionLauncher.launch(RECORD_PERMISSION)
+        return mPermissionAllowed
     }
+
+    private val activitySettingsResult =
+        registerForActivityResult(ActivityResultContracts.StartActivityForResult()) {
+            mPermissionAllowed = checkIfPermissionGranted(context)
+        }
+
+    private fun setAnimation(
+        scale: Float,
+        icon: Int,
+        colorFilter: Int,
+        background: Int,
+        rotation: Float
+    ) {
+        bdn.recordFab.animate()
+            .rotationBy(rotation)
+            .setDuration(100)
+            .scaleX(0.7f)
+            .scaleY(0.7f)
+            .withEndAction {
+                bdn.recordFab.setImageResource(icon) // setting other icon
+                bdn.recordFab.setColorFilter(getResources().getColor(colorFilter))
+                bdn.recordFab.backgroundTintList = ColorStateList.valueOf(
+                    getResources().getColor(
+                        background
+                    )
+                )
+                //Shrink Animation
+                bdn.recordFab.animate()
+                    .rotationBy(rotation) //Complete the rest of the rotation
+                    .setDuration(100)
+                    .scaleX(scale) //Scaling back to what it was
+                    .scaleY(scale)
+                    .start()
+            }
+            .start()
+    }
+
 }
---
+++ app/src/main/java/com/vkpriesniakov/voicerecorder/utils/Utils.kt
@@ -0,0 +1,46 @@
+package com.vkpriesniakov.voicerecorder.utils
+
+import android.app.Activity
+import android.content.Context
+import android.content.Intent
+import android.content.pm.PackageManager
+import android.net.Uri
+import android.provider.Settings
+import android.view.View
+import androidx.activity.result.ActivityResultLauncher
+import androidx.core.content.ContextCompat
+import com.google.android.material.snackbar.Snackbar
+import com.vkpriesniakov.voicerecorder.databinding.FragmentRecordBinding
+
+class Utils {
+    companion object {
+
+        fun showSettingsSnackbar(
+            binding: FragmentRecordBinding,
+            activity: Activity,
+            activityResult: ActivityResultLauncher<Intent>
+        ) {
+            val snackbar = Snackbar.make(
+                binding.root,
+                "Please, allow recording",
+                Snackbar.LENGTH_LONG
+            )
+            snackbar.setAction("Settings", View.OnClickListener {
+                val intent = Intent()
+                intent.action = Settings.ACTION_APPLICATION_DETAILS_SETTINGS
+                val uri: Uri =
+                    Uri.fromParts("package", (activity).packageName, null)
+                intent.data = uri
+                activityResult.launch(intent)
+            })
+            snackbar.show()
+        }
+
+        fun checkIfPermissionGranted(context: Context?): Boolean {
+            return ContextCompat.checkSelfPermission(
+                context as Context,
+                RECORD_PERMISSION
+            ) == PackageManager.PERMISSION_GRANTED
+        }
+    }
+}
